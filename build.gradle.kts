9dcdfffd34e5d308364545be92d573756814e387
Jinny <jhs7251@naver.com>
2025-03-13 20:37:38 +0900

    03/13 커밋

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,9 @@
 plugins {
-    id("java")
-    id("org.springfamework.boot") version "3.3.1"
-    id("id.spring.dependency-management") version "1.1.5"
+    id("org.springframework.boot") version "3.3.1"
+    id("io.spring.dependency-management") version "1.1.5"
+    kotlin("jvm") version "1.6.10"
+    kotlin("plugin.spring") version "1.6.10"
 }
-
-group = "org.fastcampus"
-version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
